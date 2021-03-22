7b5801ca48e3a1a87fd5dbad2d57e6ab17f634f2
Richard Seward <raseward14@gmail.com>
2021-03-21 20:28:45 -0600

    fig font and chalk both installed and working

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,5 +1,9 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+// for coloring output
+const chalk = require('chalk');
+// for display banner
+const figlet = require('figlet');
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -15,13 +19,30 @@ const connection = mysql.createConnection({
     database: 'employeetracker_db',
   });
 
+  console.log(
+    chalk.yellow(
+      figlet.textSync('Employee Manager', { 
+        font: 'bulbhead',
+        horizontalLayout: 'full',
+        verticalLayout: 'full' 
+      }, function(err, data) {
+        if (err) {
+          console.log('There is a problem...');
+          console.dir(err);
+          return;
+        }
+        console.log(data);
+      })
+    )
+  );
 
+  const runStart = () => {
+    console.log('up and running');
+  };
 
   connection.connect((err) => {
     if (err) throw err;
     runStart();
   });
 
-  const runStart = () => {
-    console.log('up and running');
-  };
+  
